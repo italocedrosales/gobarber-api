4ede2cf851788c0b7f3926f16e7599e58de96a19
Italo Cedro <italo.cedro.sales@gmail.com>
2020-10-18 16:28:09 -0300

    Configuring nodejs debug

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,15 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/users', (request, response) => {
-  return response.json({ message: 'Hello world' });
+routes.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  return response.json(user);
 });
 
 export default routes;
